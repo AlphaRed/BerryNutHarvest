c76b1a7cc04e163b8ab5385678b78dacf5b70adc
Isaac Volk <isaacvolk3@gmail.com>
2023-11-06 22:00:42 -0500

    Update window size/resolution and added test scene

--- menu.gd
+++ menu.gd
@@ -4,7 +4,7 @@ func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
-	#get_tree().change_scene() 
+	get_tree().change_scene_to_file("res://tiny_mouse_house.tscn")
 	pass
 
 func _on_quit_button_pressed():
